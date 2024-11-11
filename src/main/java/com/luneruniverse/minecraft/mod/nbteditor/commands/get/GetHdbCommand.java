625c31aa305f971b93562bd1f83c6785a34d4060
mega12345mega <55769873+mega12345mega@users.noreply.github.com>
2024-11-11 02:30:50 -0800

    Fixed ClickSlotC2SPacket bug, fixed /get hdb error messages, fixed keyPressed bug with find & replace

--- src/main/java/com/luneruniverse/minecraft/mod/nbteditor/commands/get/GetHdbCommand.java
+++ src/main/java/com/luneruniverse/minecraft/mod/nbteditor/commands/get/GetHdbCommand.java
@@ -39,15 +39,19 @@ public class GetHdbCommand extends ClientCommand {
 	@Override
 	public void register(LiteralArgumentBuilder<FabricClientCommandSource> builder, String path) {
 		builder.then(literal("search").then(argument("query", StringArgumentType.greedyString()).executes(context -> {
-					HeadAPI.openSearchDatabase(context.getArgument("query", String.class));
+					if (HeadAPI.checkUpdated())
+						HeadAPI.openSearchDatabase(context.getArgument("query", String.class));
 					return Command.SINGLE_SUCCESS;
 				})))
 				.then(literal("tagsearch").then(argument("query", StringArgumentType.greedyString()).executes(context -> {
-					HeadAPI.openTagSearchDatabase(context.getArgument("query", String.class));
+					if (HeadAPI.checkUpdated())
+						HeadAPI.openTagSearchDatabase(context.getArgument("query", String.class));
 					return Command.SINGLE_SUCCESS;
 				})))
 				.then(literal("id").then(argument("id", IntegerArgumentType.integer(1))
 						.then(argument("amount", IntegerArgumentType.integer(1)).executes(context -> {
+							if (!HeadAPI.checkUpdated())
+								return Command.SINGLE_SUCCESS;
 							Head head = HeadAPI.getHeadByID(context.getArgument("id", Integer.class));
 							if (head == null)
 								context.getSource().sendFeedback(TextInst.translatable("nbteditor.hdb.head_not_found"));
@@ -56,6 +60,8 @@ public class GetHdbCommand extends ClientCommand {
 							return Command.SINGLE_SUCCESS;
 						}))
 					.executes(context -> {
+						if (!HeadAPI.checkUpdated())
+							return Command.SINGLE_SUCCESS;
 						Head head = HeadAPI.getHeadByID(context.getArgument("id", Integer.class));
 						if (head == null)
 							context.getSource().sendFeedback(TextInst.translatable("nbteditor.hdb.head_not_found"));
@@ -64,6 +70,8 @@ public class GetHdbCommand extends ClientCommand {
 						return Command.SINGLE_SUCCESS;
 					})))
 				.then(literal("all").then(argument("category", EnumArgumentType.options(Category.class)).executes(context -> {
+					if (!HeadAPI.checkUpdated())
+						return Command.SINGLE_SUCCESS;
 					Category category = context.getArgument("category", Category.class);
 					ItemStack shulker = ShulkerBoxBlock.getItemStack(MainUtil.getDyeColor(category.getColor()));
 					shulker.manager$setCustomName(TextInst.of(Formatting.RESET.toString() + category.getColor() + Formatting.BOLD + category.getTranslatedName().toUpperCase()));
@@ -72,6 +80,8 @@ public class GetHdbCommand extends ClientCommand {
 					MainUtil.getWithMessage(shulker);
 					return Command.SINGLE_SUCCESS;
 				})).then(literal("search").then(argument("query", StringArgumentType.greedyString()).executes(context -> {
+					if (!HeadAPI.checkUpdated())
+						return Command.SINGLE_SUCCESS;
 					String query = context.getArgument("query", String.class);
 					ItemStack shulker = new ItemStack(Items.BROWN_SHULKER_BOX);
 					shulker.manager$setCustomName(TextInst.of(Formatting.RESET.toString() + Formatting.GOLD + Formatting.BOLD + TextInst.translatable("nbteditor.hdb.search").getString() + ": " + query));
